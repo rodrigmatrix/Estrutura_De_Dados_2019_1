04a97bef811b179f73ebc6a091c77f5f26c135ed
Rodrigmatrix <rodrigmatrix@hotmail.com>
2019-03-10 18:37:03 -0300

    fill maze

--- 03_labirinto/labirinto.c
+++ 03_labirinto/labirinto.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void fill_maze(char maze[15][15]){
+const char wall = 'H';
+const char path = '#';
 
+void fill_maze(char maze[15][15], int l, int c){
+    if(l > 15 || c > 15){
+        return;
+    }
+    else if(maze[l][c] == wall){
+        return;
+    }
+    
 }
 
 void print_maze(char maze[15][15]){
@@ -14,13 +24,15 @@ void print_maze(char maze[15][15]){
 }
 int main(){
     char maze[15][15];
+    int l;
+    int c;
     for(int i = 0; i < 15; i++){
         for(int j = 0; j < 15; j++){
             if((i == 0 || j == 0) || (i == 14 || j == 14)){
-                maze[i][j] = 'D';
+                maze[i][j] = wall;
             }
             else{
-                maze[i][j] = '#';
+                maze[i][j] = path;
             }
         }
     }
