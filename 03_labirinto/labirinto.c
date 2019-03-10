dfe09170248f6617ed3f09df1f00973e70761528
Rodrigmatrix <rodrigmatrix@hotmail.com>
2019-03-10 18:27:28 -0300

    Fill and print

--- 03_labirinto/labirinto.c
+++ 03_labirinto/labirinto.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
-int main(){
+void fill_maze(char maze[15][15]){
 
+}
+
+void print_maze(char maze[15][15]){
+    for(int i = 0; i < 15; i++){
+        for(int j = 0; j < 15; j++){
+            printf("%c", maze[i][j]);
+        }
+        puts("");
+    }
+}
+int main(){
+    char maze[15][15];
+    for(int i = 0; i < 15; i++){
+        for(int j = 0; j < 15; j++){
+            if((i == 0 || j == 0) || (i == 14 || j == 14)){
+                maze[i][j] = 'D';
+            }
+            else{
+                maze[i][j] = '#';
+            }
+        }
+    }
+    print_maze(maze);
 }
